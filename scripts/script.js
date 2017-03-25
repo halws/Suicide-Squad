a785ae12a298c350148c9a556aa68c77c1ac6471
Nazar <kotsuyba@gmail.com>
2017-03-25 17:59:35 +0100

    Opracowac wszystkie zdjecia; Dopasowac tekst; skonczyc section tiles

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,19 @@
 $(function checkPosition() {
-    if (window.matchMedia('(min-width: 767px)').matches) {
-        var $banner = $('#banner');
-        var $paralaxSpeed = $banner.data('speed');
+    if (window.matchMedia("(min-width: 767px)").matches) {
+        var $banner = $("#banner");
+        var $paralaxSpeed = $banner.data("speed");
         var $window = $(window);
-        var $posY = '100vh' - ($window.scrollTop() / $paralaxSpeed);
-        var $coords = '50% ' + $posY + 'px';
+        var $posY = "100vh" - $window.scrollTop() / $paralaxSpeed;
+        var $coords = "50% " + $posY + "px";
         $(this).scroll(function() {
-
             $($banner).css({
                 backgroundPosition: $coords
             });
-
         });
     }
 });
 
 $(function() {
-    var $firstSpan = $('#tiles .row:first .col span');
-    var firstImageWidth = $('#tiles .row:first .col img').width();
-    $firstSpan.width(firstImageWidth);
+    var Span = $("#tiles .row .col span");
+    Span.width($("#tiles .row .col img").width());
 });
